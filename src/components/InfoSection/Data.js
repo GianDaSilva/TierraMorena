e074ef935c5b57b4d5fd146b4c88af4c9bf1041d
GianDaSilva <gianidasilva26@gmail.com>
2021-10-30 14:41:09 -0300

    Avances

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,16 +1,57 @@
+ import foto1 from '../../images/svg-1.svg'
+ import foto2 from '../../images/svg-2.svg'
+ import foto3 from '../../images/svg-3.svg'
+
 export const homeObjOne = {
-    id: 'about', 
+    id: 'about',
     lightBg: false,
     lightText: true,
-    lighTextDesc: true,
+    lightTextDesc: true,
     topLine: 'Premium Bank',
     headline: 'Unlimited Transactions with zero fees',
-    description: 'Get access to our exclusive app that allows you to send unlimited transactions without geeting charged any fees',
-    buttonLabel: 'Get started',
+    description:
+      'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
     imgStart: false,
-    img: require('../../images/undraw_off_road_9oae.png'),
-    alt: 'Car',
-    dark: true,
+    img: foto1,
+    alt: 'Piggy',
+    dark: false,
     primary: true,
-    darkText: false,
-};
+    darkText: false
+  };
+
+  export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account at any time',
+    description:
+      'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: foto2,
+    alt: 'Chart',
+    dark: false,
+    primary: false,
+    darkText: true
+  };
+
+  export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our Team',
+    headline: 'Creating an account is extremely easy',
+    description:
+      "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: foto3,
+    alt: 'Chart2',
+    dark: false,
+    primary: false,
+    darkText: true
+  };
---
+++ src/components/Services/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <ServicesContainer id="services">
+            <ServicesH1>Our services</ServicesH1>
+                <ServicesWrapper>
+                    <ServicesCard>
+                    <ServicesIcon src={Icon1}/>
+                    <ServicesH2>Reduce expenses</ServicesH2>
+                    <ServicesP>We help reduce your fess and increase your overall revenue.</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon2}/>
+                    <ServicesH2>Virtual offices</ServicesH2>
+                    <ServicesP>You can acces our platform online  anywhere in the world.</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon3}/>
+                    <ServicesH2>Premium benefits</ServicesH2>
+                    <ServicesP>Unlock our special membership card that returns 5% cash back.</ServicesP>
+                </ServicesCard>
+                
+            </ServicesWrapper>
+        </ServicesContainer>
+    )
+}
+
+export default Services
